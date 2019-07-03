7879abd18339ce586c54e745fb18324860b7a162
OgorodnikSergey <osv8487@gmail.com>
2019-07-04 01:48:08 +0300

    HomeWork521v1

--- app/src/main/java/ru/ogorodnik/homework521/MainActivity.java
+++ app/src/main/java/ru/ogorodnik/homework521/MainActivity.java
@@ -36,49 +36,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if (!login.getText().toString().equals("") && !password.getText().toString().equals("")) {
+                if (!login.getText().toString().equals("") || !password.getText().toString().equals("")) {
                     //------------------------------------Загружаем логин
-                    BufferedReader br = null;
-                    try {
-                        br = new BufferedReader(new InputStreamReader(
-                                openFileInput(FILE_LOGIN)));
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                    String strLog = "";
-                    // читаем содержимое
-                    while (true) {
-                        try {
-                            if (((strLog = br.readLine()) != null)) break;
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
+                    String strLog = readLogin();
                     //------------------------------------Загружаем пароль
-                    br = null;
-                    try {
-                        br = new BufferedReader(new InputStreamReader(
-                                openFileInput(FILE_PASSWORD)));
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                    String strPassword = "";
-                    // читаем содержимое
-                    while (true) {
-                        try {
-                            if (((strPassword = br.readLine()) != null)) break;
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
+                    String strPassword = readPassword();
+
                     if (password.getText().toString().equals (strPassword) & login.getText().toString().equals(strLog)){
                         exampleText.setText("Поздравляю вы ввели правильный Логин и Пароль");
                         Toast.makeText(MainActivity.this, "Вы вошли в систему", Toast.LENGTH_LONG).show();
                     } else {
 
-                        exampleText.setText (getText(R.string.messageError));
+                        exampleText.setText (getText(R.string.messageErrorLogin));
+                        Toast.makeText(MainActivity.this, getText(R.string.messageErrorToastLogin), Toast.LENGTH_LONG).show();
                     }
-
                 }
             }
         });
@@ -87,47 +58,80 @@ public class MainActivity extends AppCompatActivity {
         registrationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // --------------------------------- сохраняем логин
-                String text = login.getText().toString();
-                FileOutputStream fos = null;
-                try {
-                    fos = openFileOutput(FILE_LOGIN, MODE_PRIVATE);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    fos.write(text.getBytes());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                login.getText().clear();
-                // --------------------------------- сохраняем пароль
-                text = password.getText().toString();
-                fos = null;
-                try {
-                    fos = openFileOutput(FILE_PASSWORD, MODE_PRIVATE);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    fos.write(text.getBytes());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    if (fos != null) {
-                        try {
-                            fos.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
+                if (!login.getText().toString().equals("") && !password.getText().toString().equals("")) {
+
+                    // --------------------------------- сохраняем логин
+                    String text = login.getText().toString();
+                    saveData(FILE_LOGIN, text);
+                    login.getText().clear();
+                    // --------------------------------- сохраняем пароль
+                    text = password.getText().toString();
+                    saveData(FILE_PASSWORD, text);
                     password.getText().clear();
                     //-----------------------------------
                     exampleText.setText("Вы зарегистрированы");
                     Toast.makeText(MainActivity.this, "Файлы сохранены " + getFilesDir(), Toast.LENGTH_LONG).show();
+                } else {
+                    exampleText.setText (getText(R.string.messageErrorRegistr));
+                    Toast.makeText(MainActivity.this, getText(R.string.messageErrorToastRegistr), Toast.LENGTH_LONG).show();
                 }
             }
         });
 
+}
+//------------------------------------------------------------
+    private String readLogin() {
+        return readLineFromFile(FILE_LOGIN);
     }
+//------------------------------------------------------------
+    private String readPassword() {
+        return readLineFromFile(FILE_PASSWORD);
+    }
+//------------------------READ FILE---------------------------
+    private String readLineFromFile(String fileName) {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(
+                    openFileInput(fileName)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String Stroka = "";
+        // читаем содержимое
+        while (true) {
+            try {
+                if (((Stroka = br.readLine()) != null)) {
+                    break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Stroka;
+    }
+//------------------------------------------------------------
+    private void saveData (String fileName, String text){
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(fileName, MODE_PRIVATE);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            fos.write(text.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
